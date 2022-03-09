43ef990d687a0309652a339df4e1562a34ad1b76
rahul-53 <yrahul0620@gmail.com>
2022-03-09 23:02:45 +0530

    adapter class created

---
+++ app/src/main/java/com/rahul/saveoclone/adapters/HomeAdapter.kt
@@ -0,0 +1,21 @@
+package com.rahul.saveoclone.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HomeAdapter:RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+    inner class HomeViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
